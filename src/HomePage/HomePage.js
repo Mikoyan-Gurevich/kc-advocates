58609fb3eb50352101fde0d766c39ad39521256d
auravsing <gauravnegi159012@gmail.com>
2018-10-29 18:52:47 +0530

    Minor additions

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './HomePage.css';
+
+class HomePage extends Component {
+
+    render() {
+        return (
+
+            <div className="MainPage">
+                <div className="MainTitle"></div>
+                <div className="WorkingAreas">ONLY CLIENT MATTER
+                                <button class="sector_Button">Sectors</button>
+                    <button class="prac_areas">Practice Areas</button>
+                </div>
+                <div className="WorkingAreas_two"><h1>CRIMINAL DEFENCE LAW FIRM</h1>
+                    <p>To be the best in law defence</p>
+                    <p> To be the best in law defence</p></div>
+            </div>
+
+        );
+    }
+}
+
+export default HomePage;
---
+++ src/PracticeAreas/PracticeAreas.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './PracticeAreas.css';
+
+class PracticeAreas extends Component {
+
+
+
+    render() {
+
+        return (
+            <div className='PracAreas'>Prac areas page
+
+            </div>
+        );
+    }
+}
+
+export default PracticeAreas;
